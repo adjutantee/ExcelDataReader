ba40a2bc30ba7d52b6655b20d5f61de2c5b6fdc5
adjutantee <romashilov3233@gmail.com>
2022-03-29 17:38:49 +0300

    save comit

--- ExcelReading/Program.cs
+++ ExcelReading/Program.cs
@@ -13,44 +13,21 @@ class Program
         FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
         IExcelDataReader excelReader;
 
-        // Чтение Excel файла
         if (Path.GetExtension(filePath).ToUpper() == ".XLS")
         {
-            // Если фомат файла = .XLS
             excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
         }
         else
-        {
-            // Или формат файла = .XLSX
+        {      
             excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
         }
 
         DataSet result = excelReader.AsDataSet();
 
-        // excelReader.IsFirstRowAsColumnNames = false;
-
+        //excelReader.IsFirstRowAsColumnNames = false;
         DataTable dt = result.Tables[0];
         Console.WriteLine(result.Tables[0].Rows.Count);
         Console.WriteLine(result.Tables[0].Columns.Count);
-
         excelReader.Close();
-        //using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
-        //{
-        //    IExcelDataReader reader;
-        //    reader = ExcelDataReader.ExcelReaderFactory.CreateReader(stream);
-
-        //    var conf = new ExcelDataSetConfiguration
-        //    {
-        //        ConfigureDataTable = _ => new ExcelDataTableConfiguration
-        //        {
-        //            UseHeaderRow = true
-        //        }
-        //    };
-        //    var dataSet = reader.AsDataSet(conf);
-
-        //    // Now you can get data from each sheet by its index or its "name"
-        //    var dataTable = dataSet.Tables[0];
-        //    Console.WriteLine(dataSet);
-        //}
     }
 }
